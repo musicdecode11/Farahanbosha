793ad1fd122ead77a13a2e9a83aee7ed67d912d0
musicdecode11 <musicdecode809@gmail.com>
2025-09-09 09:50:12 +0530

    index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '6654600041:AAFDdP_tA29nF_lvFCAHv2CEQsDh55fEaj4'
-const id = '6563078470'
+const token = '8215977113:AAGVCVYgvfwNORJK2w7TT_Ay8-OhL7tm9bk'
+const id = '7804262619'
 const address = 'https://www.google.com'
 
 const app = express();
